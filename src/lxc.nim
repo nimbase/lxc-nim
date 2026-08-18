7afbf0f388fc5400bf58e8164f2962157f0942f4
George Lemon <georgelemon@protonmail.com>
2026-08-18 22:33:58 +0300

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/lxc.nim
+++ src/lxc.nim
@@ -3,8 +3,7 @@
 # LGPL-2.1-only
 
 when defined(linux):
-  {.passL: gorge("pkg-config --libs liblxc").}
-  {.passC: gorge("pkg-config --cflags liblxc").}
+  {.passL: "-llxc".}
 
 import ./lxc/bindings/[common, container, attach, snapshot, free_api]
 import ./lxc/api
